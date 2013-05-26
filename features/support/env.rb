050498378a770223cd8a5cc9fc749460be0c0197
Josh Cheek <josh.cheek@gmail.com>
2013-05-26 01:18:41 -0500

    Configure Haiti

---
+++ features/support/env.rb
@@ -0,0 +1,5 @@
+require 'haiti'
+Haiti.configure do |config|
+  config.proving_grounds_dir = File.expand_path '../../../proving_grounds', __FILE__
+  config.bin_dir             = File.expand_path '../../../bin',             __FILE__
+end
