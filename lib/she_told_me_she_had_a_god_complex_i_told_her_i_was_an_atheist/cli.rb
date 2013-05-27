55e63882b8bb50faf6d7e8da81694b836db2211f
Josh Cheek <josh.cheek@gmail.com>
2013-05-27 14:01:42 -0500

    Create Encryption/Decryption classes

--- lib/she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/cli.rb
+++ lib/she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/cli.rb
@@ -6,24 +6,53 @@ module SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist
       self.env  = env
     end
 
+    class Encryption
+      class Decrypt
+        def self.call(*args)
+          new(*args).call
+        end
+        attr_accessor :encrypted_passwords, :master_password
+        def initialize(encrypted_passwords, master_password)
+          self.encrypted_passwords = encrypted_passwords
+          self.master_password     = master_password
+        end
+
+        def call
+          Encryptor.decrypt encrypted_passwords, key: Digest::SHA256.hexdigest(master_password)
+        rescue OpenSSL::OpenSSLError
+        end
+      end
+
+      class Encrypt
+        def self.call(*args)
+          new(*args).call
+        end
+        attr_accessor :unencrypted_passwords, :master_password
+        def initialize(unencrypted_passwords, master_password)
+          self.unencrypted_passwords = unencrypted_passwords
+          self.master_password       = master_password
+        end
+
+        def call
+          Encryptor.encrypt unencrypted_passwords, key: Digest::SHA256.hexdigest(master_password)
+        end
+      end
+    end
+
     def call
       password_filename = env.fetch "she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist",
                                     "#{env['HOME']}/.she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist"
-
       if argv.include? '--set'
         if File.exist? password_filename
-          old_password   = io.password 'enter your old passord: '
-          secret_key     = Digest::SHA256.hexdigest old_password
-          encrypted_file = File.read password_filename
-          begin
-            decrypted_file = Encryptor.decrypt encrypted_file, key: secret_key
-            new_password   = io.password 'enter your new passord: '
-            secret_key     = Digest::SHA256.hexdigest new_password
-            encrypted_file = Encryptor.encrypt decrypted_file, key: secret_key
-            File.open(password_filename, 'w') { |f| f.write encrypted_file }
+          old_password        = io.password 'enter your old passord: '
+          decrypted_passwords = Encryption::Decrypt.call File.read(password_filename), old_password
+          if decrypted_passwords
+            new_password        = io.password 'enter your new passord: '
+            encrypted_passwords = Encryption::Decrypt.call decrypted_passwords, new_password
+            File.open(password_filename, 'w') { |f| f.write encrypted_passwords }
             io.success "your master password has been set"
             0
-          rescue OpenSSL::OpenSSLError
+          else
             $stderr.puts "Incorrect master password" # should delegate to io.failure
             1
           end
