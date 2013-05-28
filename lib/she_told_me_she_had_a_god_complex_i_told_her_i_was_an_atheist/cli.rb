9d5c5ac386ed62e0c27a8f0aab77807a593d6e52
Josh Cheek <josh.cheek@gmail.com>
2013-05-28 07:59:52 -0500

    Refactor CLI

--- lib/she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/cli.rb
+++ lib/she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/cli.rb
@@ -10,17 +10,11 @@ module SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist
     def call
       password_filename = env.fetch "she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist",
                                     "#{env['HOME']}/.she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist"
-      if argv.include? '--set'
-        # try refactoring to this later
-        # action = SetMasterPassword
-        # action = ResetMasterPassword if File.exist? password_filename
-        # action.call io, password_filename
-        if File.exist? password_filename
-          ResetMasterPassword.call io, password_filename
-        else
-          SetMasterPassword.call io, password_filename
-        end
-      elsif argv.include? '--add'
+      if argv == ['--set']
+        action = SetMasterPassword
+        action = ResetMasterPassword if File.exist? password_filename
+        action.call io, password_filename
+      elsif argv == ['--add']
         AddPassword.call io, password_filename
       else
         raise "Don't know what to do with ARGV: #{ARGV.inspect}"
