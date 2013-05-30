50407981064a8205a572300a108a43545cf225dd
Josh Cheek <josh.cheek@gmail.com>
2013-05-29 21:37:06 -0500

    Remove command_line_binary files for use cases that were turned into domain objects

--- lib/she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary.rb
+++ lib/she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary.rb
@@ -1,7 +1,5 @@
 require 'she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist'
-require 'she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary/set_master_password'
-require 'she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary/reset_master_password'
-require 'she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary/add_password'
+require 'she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary/interface'
 require 'she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary/get_password'
 require 'she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary/high_line_io'
 
@@ -12,12 +10,21 @@ module SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist
     def call
       password_filename = env.fetch "she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist",
                                     "#{env['HOME']}/.she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist"
-      if argv == ['--set']
-        action = SetMasterPassword
-        action = ResetMasterPassword if File.exist? password_filename
-        action.call io, password_filename
+      if (argv == ['--set']) && File.exist?(password_filename)
+        interface = Interface.new io, password_filename do
+          io.success "your master password has been set"
+        end
+        SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist::ResetMasterPassword.call interface
+      elsif argv == ['--set']
+        interface = Interface.new io, password_filename do
+          io.success "your master password has been set"
+        end
+        SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist::SetMasterPassword.call interface
       elsif argv == ['--add']
-        AddPassword.call io, password_filename
+        interface = Interface.new io, password_filename do
+          io.success "your password for '#{interface.get_name}' is now being stored"
+        end
+        SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist::AddPassword.call interface
       else
         GetPassword.call io, password_filename
       end
