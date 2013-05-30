94d46ac582370f2191cd05fc51ec34d14907aefa
Josh Cheek <josh.cheek@gmail.com>
2013-05-29 21:43:50 -0500

    Refactor command line binary

--- lib/she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary.rb
+++ lib/she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist/command_line_binary.rb
@@ -8,23 +8,12 @@ module SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist
     Callable.call self, :stdin, :stdout, :stderr, :argv, :env
 
     def call
-      password_filename = env.fetch "she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist",
-                                    "#{env['HOME']}/.she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist"
-      if (argv == ['--set']) && File.exist?(password_filename)
-        interface = Interface.new io, password_filename do
-          io.success "your master password has been set"
-        end
-        SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist::ResetMasterPassword.call interface
+      if argv == ['--set'] && File.exist?(password_filename)
+        use_case(ResetMasterPassword) { "your master password has been set" }
       elsif argv == ['--set']
-        interface = Interface.new io, password_filename do
-          io.success "your master password has been set"
-        end
-        SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist::SetMasterPassword.call interface
+        use_case(SetMasterPassword) { "your master password has been set" }
       elsif argv == ['--add']
-        interface = Interface.new io, password_filename do
-          io.success "your password for '#{interface.get_name}' is now being stored"
-        end
-        SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist::AddPassword.call interface
+        use_case(AddPassword) { |interface| "your password for '#{interface.get_name}' is now being stored" }
       else
         GetPassword.call io, password_filename
       end
@@ -32,6 +21,16 @@ module SheToldMeSheHadAGodComplexIToldHerIWasAnAtheist
 
     def io
       @io ||= HighLineIO.new stdin, stdout, stderr
+    end
+
+    def password_filename
+      @password_filename ||= env.fetch "she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist",
+                                       "#{env['HOME']}/.she_told_me_she_had_a_god_complex_i_told_her_i_was_an_atheist"
+    end
+
+    def use_case(use_case, &message)
+      interface = Interface.new(io, password_filename) { io.success message.call interface }
+      use_case.call interface
     end
   end
 end
